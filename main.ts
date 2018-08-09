0bb8a8ccab3e2c735c0b741f2429a6d11c1b311f
Krishika Khemani <kk@consol.ae>
2018-08-09 11:42:24 +0400

    playing around with functions

---
+++ main.ts
@@ -0,0 +1,16 @@
+interface Food {
+    name: string;
+    calories: number;
+}
+
+function speak(food: Food): void{
+  console.log("Our " + food.name + " has " + food.calories + " grams.");
+}
+
+
+var ice_cream = {
+  name: "ice cream",
+  calories: 200
+}
+
+speak(ice_cream);
